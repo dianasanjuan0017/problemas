8b8e73295ecd06fefc068dd83eac8fd403122967
dianasanjuan0017 <20230019@uthh.edu.mx>
2025-07-12 23:09:09 -1100

    Agregando archivos nuevos

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name='problemas',
-    version='0.1',
-    packages=find_packages(),
-    install_requires=[],
-    author='Diana Sanjuan',
-    description='Problemas de optimización con restricciones',
+    name='problemas_opt',  # Nombre del paquete (puedes elegir uno único)
+    version='0.0.1',      # Versión inicial
+    packages=find_packages(), # Busca subdirectorios con __init__.py
+    py_modules=['problemas'], # ¡Importante! Especifica que 'problemas.py' es un módulo
+    install_requires=[
+        'numpy',
+    ],
+    author='Diana Sanjuan', # Tu nombre
+    description='Clases de problemas de optimización para DE con restricciones',
+    long_description=open('README.md').read() if 'README.md' else '', # Opcional: descripción larga desde README
+    long_description_content_type='text/markdown',
+    url='https://github.com/dianasanjuan0017/problemas', # Tu URL de GitHub
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
 )
