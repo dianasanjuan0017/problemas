b1b9b335bc7abf49f4aa7c9a05ec09d63a1d79cc
dianasanjuan0017 <20230019@uthh.edu.mx>
2025-07-12 22:51:22 -1100

    Agregando archivos nuevos

---
+++ setup.py
@@ -0,0 +1,11 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='problemas',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    author='Diana Sanjuan',
+    description='Problemas de optimización con restricciones',
+)
